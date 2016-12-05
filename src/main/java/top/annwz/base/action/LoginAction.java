d64c6a87ddb8d1d2d2f1c68070c208df41c59906
吴华辉 <wuhuahui@iscs.com.cn>
2016-12-05 17:19:05 +0800

    返回修改

--- src/main/java/top/annwz/base/action/LoginAction.java
+++ src/main/java/top/annwz/base/action/LoginAction.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import top.annwz.base.uitl.AbsResponse;
 import top.annwz.base.uitl.Converter;
+import top.annwz.base.uitl.ReqUtil;
 
 import javax.xml.ws.Response;
 import java.util.HashMap;
+import java.util.Observable;
 
 /**
  * Created by Wuhuahui on 2016/12/5.
@@ -18,12 +20,14 @@ import java.util.HashMap;
 @RequestMapping("/login")
 public class LoginAction extends BasicAction {
 	@RequestMapping("/login")
-	public AbsResponse<HashMap<Object, String>> initLogin(@RequestBody HashMap<String, Object> params) {
-		AbsResponse<HashMap<Object, String>> abs = new AbsResponse<HashMap<Object, String>>();
+	public AbsResponse<HashMap<String, Object>> initLogin(@RequestBody HashMap<String, Object> params) {
+		AbsResponse<HashMap<String, Object>> abs = new AbsResponse<HashMap<String, Object>>();
+		HashMap<String, Object> map = new HashMap<String, Object>();
 		String userId = Converter.getString(params, "userId");
 		logger.debug("登录" + userId);
-//		map.put("userId", userId);
-		abs.setCode(0);
+		map.put("userId", userId);
+		ReqUtil.setErrAbs(abs, 1001 , "error");
+		abs.setData(map);
 		return abs;
 	}
 }
