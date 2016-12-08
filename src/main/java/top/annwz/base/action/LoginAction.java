7d1c981a967ffacb5dfadc1f7d6ad3dfd6b715a5
吴华辉 <wuhuahui@iscs.com.cn>
2016-12-08 19:24:05 +0800

    添加加密工具类

--- src/main/java/top/annwz/base/action/LoginAction.java
+++ src/main/java/top/annwz/base/action/LoginAction.java
@@ -1,5 +1,6 @@
 package top.annwz.base.action;
 
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,6 @@ import top.annwz.base.uitl.ReqUtil;
 import top.annwz.base.uitl.SpringContextHolder;
 
 import javax.annotation.Resource;
-import javax.xml.ws.Response;
 import java.util.HashMap;
 import java.util.Observable;
 import java.util.UUID;
@@ -22,21 +22,18 @@ import java.util.UUID;
  */
 @Controller
 @ResponseBody
-@RequestMapping("/login")
+@RequestMapping("/api")
 public class LoginAction extends BasicAction {
 	@Resource
 	IBaUserService baUserService;
 	@RequestMapping("/login")
 	public AbsResponse<HashMap<String, Object>> initLogin(@RequestBody HashMap<String, Object> params) {
 		AbsResponse<HashMap<String, Object>> abs = new AbsResponse<HashMap<String, Object>>();
-		HashMap<String, Object> map = new HashMap<String, Object>();
+		HashMap<String, Object> map = new HashMap <String, Object>();
 		String userId = Converter.getString(params, "userId");
 		BaUser baUser = baUserService.getUser(Long.valueOf(userId));
-		String ukid = UUID.randomUUID().toString(); //获取唯一的ukid
-		logger.debug("登录" + ukid);
-		map.put("userId", ukid);
-//		ReqUtil.setErrAbs(abs, 1001 , "error");
-//		map.put("bean",SpringContextHolder.getBean(this.getClass()));
+
+		map.put("userId", userId);
 		map.put("user", baUser);
 		abs.setData(map);
 
---
+++ src/main/java/top/annwz/base/service/imp/RegisteredService.java
@@ -0,0 +1,7 @@
+package top.annwz.base.service.imp;
+
+/**
+ * Created by Wuhuahui on 2016/12/8.
+ */
+public class RegisteredService {
+}
