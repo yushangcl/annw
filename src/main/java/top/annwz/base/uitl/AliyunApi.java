c244113d18fa3b9aac8d9697cc678d21c5bf106d
吴华辉 <wuhuahui@iscs.com.cn>
2016-12-16 14:17:46 +0800

    发送短信 修改bug

--- src/main/java/top/annwz/base/uitl/AliyunApi.java
+++ src/main/java/top/annwz/base/uitl/AliyunApi.java
@@ -1,5 +1,6 @@
 package top.annwz.base.uitl;
 
+import com.alibaba.fastjson.JSONObject;
 import org.apache.http.HttpResponse;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -15,7 +16,9 @@ import java.util.Map;
 public class AliyunApi {
 	private static Logger log = LogManager.getLogger(AliyunApi.class);
 
-	private static final String method = "GET";
+	private static final String methodGet = "GET";
+	private static final String methodPost = "POST";
+
 	private static final String appCode = "af8670f2f0534c469b0597b6fd834022";
 
 	/**
@@ -35,10 +38,10 @@ public class AliyunApi {
 		//最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
 		headers.put("Authorization", "APPCODE " + appCode);
 		Map<String, String > querys = new HashMap<String, String>();
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("name", name);
-		map.put("msg", code);
-		querys.put("ParamString", map.toString());
+		JSONObject jsonObj = new JSONObject();
+		jsonObj.put("name", name);
+		jsonObj.put("msg", code);
+		querys.put("ParamString", jsonObj.toJSONString());
 		querys.put("RecNum", phoneNum);
 		querys.put("SignName", SignName);
 		querys.put("TemplateCode", TemplateCode);
@@ -52,7 +55,7 @@ public class AliyunApi {
 			 * 相应的依赖请参照
 			 * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/pom.xml
 			 */
-			HttpResponse response = HttpUtils.doGet(host, path, method, headers, querys);
+			HttpResponse response = HttpUtils.doGet(host, path, methodGet, headers, querys);
 			System.out.println(response);
 			//获取response的body
 			//System.out.println(EntityUtils.toString(response.getEntity()));
@@ -63,7 +66,115 @@ public class AliyunApi {
 		return 0;
 	}
 
+	/**
+	 * {
+	 "inputs": [
+	 {
+	 "image": {
+	 "dataType": 50,
+	 "dataValue": "base64_image_string(#括号内为描述，不需上传，图片以base64编码的string)"
+	 },
+	 "configure": {
+	 "dataType": 50,
+	 "dataValue": "{"side":"face(#括号内为描述，不需上传，身份证正反面类型:face/back)"}"
+	 }
+	 }
+	 ]
+	 }
+	 * @param args
+	 */
+	public static void idCard(String[] args) {
+		String host = "https://dm-51.data.aliyun.com";
+		String path = "/rest/160601/ocr/ocr_idcard.json";
+		Map<String, String> headers = new HashMap<String, String>();
+		//最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
+		headers.put("Authorization", "APPCODE " + appCode);
+		Map<String, String> querys = new HashMap<String, String>();
+		String bodys = "{\"inputs\":[{\"image\":{\"dataType\":50,\"dataValue\":\"base64_image_string(#括号内为描述，不需上传，图片以base64编码的string)\"},\"configure\":{\"dataType\":50,\"dataValue\":\"{\\\"side\\\":\\\"face(#括号内为描述，不需上传，身份证正反面类型:face/back)\\\"}\"}}]}";
+
+
+		try {
+			/**
+			 * 重要提示如下:
+			 * HttpUtils请从
+			 * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/src/main/java/com/aliyun/api/gateway/demo/util/HttpUtils.java
+			 * 下载
+			 *
+			 * 相应的依赖请参照
+			 * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/pom.xml
+			 */
+			HttpResponse response = HttpUtils.doPost(host, path, methodPost, headers, querys, bodys);
+			System.out.println(response.toString());
+			//获取response的body
+			//System.out.println(EntityUtils.toString(response.getEntity()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void getCity(String[] args) {
+		String host = "http://jisutqybmf.market.alicloudapi.com";
+		String path = "/weather/city";
+		Map<String, String> headers = new HashMap<String, String>();
+		//最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
+		headers.put("Authorization", "APPCODE " + appCode);
+		Map<String, String> querys = new HashMap<String, String>();
+
+
+		try {
+			/**
+			 * 重要提示如下:
+			 * HttpUtils请从
+			 * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/src/main/java/com/aliyun/api/gateway/demo/util/HttpUtils.java
+			 * 下载
+			 *
+			 * 相应的依赖请参照
+			 * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/pom.xml
+			 */
+			HttpResponse response = HttpUtils.doGet(host, path, methodGet, headers, querys);
+			System.out.println(response.toString());
+			//获取response的body
+			//System.out.println(EntityUtils.toString(response.getEntity()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void getWeather(String[] args) {
+		String host = "http://jisutqybmf.market.alicloudapi.com";
+		String path = "/weather/query";
+		Map<String, String> headers = new HashMap<String, String>();
+		//最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
+		headers.put("Authorization", "APPCODE " + appCode);
+		Map<String, String> querys = new HashMap<String, String>();
+		querys.put("city", "安顺");
+		querys.put("citycode", "citycode");
+		querys.put("cityid", "cityid");
+
+
+		try {
+			/**
+			 * 重要提示如下:
+			 * HttpUtils请从
+			 * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/src/main/java/com/aliyun/api/gateway/demo/util/HttpUtils.java
+			 * 下载
+			 *
+			 * 相应的依赖请参照
+			 * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/pom.xml
+			 */
+			HttpResponse response = HttpUtils.doGet(host, path, methodGet, headers, querys);
+			System.out.println(response.toString());
+			//获取response的body
+			//System.out.println(EntityUtils.toString(response.getEntity()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+
+
 	public static void main(String[] args) {
-		sendVerificationCode("测试", "SMS_34370181", "18268006317", "123456", "whh");
+		sendVerificationCode("测试", "SMS_34285489", "18368093869", "123456", "whh");
 	}
 }
