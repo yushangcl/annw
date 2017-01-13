7522e4ae5d37e2303be2309589976d476df28bc8
吴华辉 <wuhuahui@iscs.com.cn>
2017-01-13 11:30:35 +0800

    XDW-4836 百度api测试

--- src/main/test/top/annwz/base/uitl/BaiduApiTest.java
+++ src/main/test/top/annwz/base/uitl/BaiduApiTest.java
@@ -10,7 +10,7 @@ import top.annwz.base.uitl.api.BaiduApi;
 public class BaiduApiTest {
 	@Test
 	public void getIpInfo() throws Exception {
-		JSONObject obj = BaiduApi.getIpInfo("115.236.172.146");
+		JSONObject obj = BaiduApi.getIpInfo("192.168.1.1");
 		String  city = obj.getJSONObject("retData").getString("city");
 		System.out.println(obj + city);
 	}
@@ -24,7 +24,7 @@ public class BaiduApiTest {
 	@Test
 	public void getWeather() throws Exception {
 		JSONObject obj = BaiduApi.getIpInfo("115.236.172.146");
-		String  city = obj.getJSONObject("retData").getString("district");
+		String  city = obj.getJSONObject("retData").getString("city");
 		JSONObject jsonObject = BaiduApi.getWeathe(city);
 		System.out.println(jsonObject);
 	}
@@ -34,5 +34,31 @@ public class BaiduApiTest {
 		JSONObject jsonObject = BaiduApi.getPhoneAttribution("18368093869");
 		System.out.println(jsonObject);
 	}
+
+	@Test
+	public void md5Dec() throws Exception {
+		JSONObject jsonObject = BaiduApi.md5Dec("7e73d06707f5fb75ec77cc5f2bd9bb92");
+		System.out.println(jsonObject);
+	}
+
+	@Test
+	public void getNewChannel() throws Exception {
+		JSONObject jsonObject = BaiduApi.getNewChannel();
+		System.out.println(jsonObject);
+	}
+
+	@Test
+	public void getNews() throws Exception {
+		JSONObject jsonObject = BaiduApi.getNews("5572a108b3cdc86cf39001d9", "", "", "1", "0", "0");
+		System.out.println(jsonObject);
+	}
+	@Test
+	public void getIdCardInfo() throws Exception {
+		JSONObject jsonObject = BaiduApi.getIdCardInfo("520324198901154695");
+		System.out.println(jsonObject);
+	}
+
+
+
 
 }
